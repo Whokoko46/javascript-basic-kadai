72454865ffc13d17d85e15eaeadea4925a8dd784
Whokoko46 <Alien487410118@gmail.com>
2025-07-28 19:36:31 +0900

    kadai_mdn_jsをアップロードします

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const moonLanding = new Date("July 26, 25");
+
+console.log(moonLanding.getFullYear());
+console.log('年');
+console.log(moonLanding.getMonth());
+console.log('月');
+console.log(moonLanding.getDate());
+console.log('日')
